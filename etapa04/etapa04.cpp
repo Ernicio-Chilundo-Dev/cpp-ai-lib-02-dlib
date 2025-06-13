56330fb787d163e745dd8f5b366fe28ff39416cb
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-13 23:42:16 +0200

    Implement face detection and print number of faces detected

--- etapa04/etapa04.cpp
+++ etapa04/etapa04.cpp
@@ -11,7 +11,14 @@ int main(){
     {
         // Carregar imagem
         array2d <rgb_pixel> img;
+        load_image(img, "../test.jpg");
+
+        // Obtem o detector de faces frontal
         frontal_face_detector detector  = get_frontal_face_detector();
+
+        // Detecta faces
+        std::vector<rectangle> faces = detector(img);
+        cout << "Detectadas "<<faces.size() << "face(s)\n";
     }
     catch(const std::exception& e)
     {

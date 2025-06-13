5b63b40cb50ec773275e993bb33e065b47d0e819
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-13 23:23:19 +0200

    started step 04

---
+++ etapa04/etapa04.cpp
@@ -0,0 +1,13 @@
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/image_io.h>
+#include <dlib/gui_widgets.h>
+#include <iostream>
+
+using namespace std;
+using namespace dlib;
+
+int main(){
+
+
+    return 0;
+}

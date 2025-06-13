82b795806ded549a3d1f8c5bab7f985fcb0f568e
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-14 01:56:53 +0200

    added

---
+++ etapa05/build/CMakeFiles/etapa05.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/etapa05.dir/etapa05.cpp.o"
+  "CMakeFiles/etapa05.dir/etapa05.cpp.o.d"
+  "etapa05"
+  "etapa05.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/etapa05.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

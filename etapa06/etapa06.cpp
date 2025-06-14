b6c8d08f9357e53173ea14997da2b9d736a6e468
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-14 12:06:56 +0200

    Integrate Dlib face detection with OpenCV webcam feed
    - Initialize webcam capture using OpenCV
    - Convert frames from OpenCV format to Dlib format for detection
    - Detect faces using Dlib’s frontal face detector
    - Draw bounding boxes around detected faces on the video feed
    - Display real-time video with face detection overlay
    - Exit the loop on 'q' key press and release resources properly
    - Handle webcam open failure and exceptions gracefully

--- etapa06/etapa06.cpp
+++ etapa06/etapa06.cpp
@@ -15,11 +15,46 @@ int main(){
             cerr << "Erro: nao foi possivel abrir o webcam";
             return 1;
         }
+
+        frontal_face_detector detector = get_frontal_face_detector();
+
+        while (true)
+        {
+            cv::Mat frame;
+            cap >> frame;
+            if(frame.empty())break;
+
+            // Converter de Opencv para Dlib
+            cv_image<bgr_pixel> cimg(frame);
+
+            // Detecta rostos
+            std::vector<rectangle> faces = detector(cimg);
+            // desenha retangulos nos rostos detectados
+            for(auto face : faces){
+                cv::rectangle(frame,
+                              cv::Point(face.left(), face.top()),
+                              cv::Point(face.right(), face.bottom()),
+                              cv::Scalar(0, 255, 0), 2);
+            }
+             // Mostra a imagem com deteccao
+            cv::imshow("Deteccao de faces com (Dlib + OpenCv)", frame);
+
+            // Sai com tecla q
+            if(cv::waitKey(1)=='q') break;
+
+        }
+
+        cap.release();
+        cv::destroyAllWindows();
+
+        
     }
     catch(const std::exception& e)
     {
-        std::cerr << e.what() << '\n';
+        std::cerr <<"Erro: "<< e.what() << '\n';
+        return 1;
     }
+
     
 
     return 0;

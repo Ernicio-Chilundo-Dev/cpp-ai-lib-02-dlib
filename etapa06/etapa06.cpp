a1e382e89ec56e4e19299abdaf3148ffe6f8de05
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-14 11:15:12 +0200

    step 6: Real-Time face detection (webcam)

---
+++ etapa06/etapa06.cpp
@@ -0,0 +1,13 @@
+#include <dlib/opencv.h>
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace dlib;
+
+int main(){
+    
+
+    return 0;
+}
